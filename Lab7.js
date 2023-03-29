2ffae6d7cb32be5c1a5bd564351de3a9d924761e
Jackllle <124173996+Jackllle@users.noreply.github.com>
2023-03-29 14:50:07 -0500

    Create Lab7.js

---
+++ Lab7.js
@@ -0,0 +1,27 @@
+function setup() {
+  createCanvas(400, 400);
+  
+  
+}
+
+function draw() {
+  background(220);
+  
+  let xCHORD= 20
+  let yCHORD= 20
+  let xCHORD2= 380
+  let yCHORD2= 0
+  
+for(let counter =0; counter <20; counter++){
+  fill(random(50,50,50));
+ellipse(xCHORD,yCHORD,30,30)
+xCHORD=xCHORD +30 
+yCHORD=yCHORD +30
+for(let counter =0; counter <20; counter++){
+rect(xCHORD2, yCHORD2, 30,30)  
+ xCHORD2=xCHORD2 -20 
+yCHORD2=yCHORD2 +30 
+  
+}  
+}
+}
